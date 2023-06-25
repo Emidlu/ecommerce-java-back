7a44c18b8ecc96ca712955476ac8aba24bc4d7ed
Emidlu <52872122+Emidlu@users.noreply.github.com>
2023-06-25 18:38:41 -0300

    Sacada importacion no necesaria

--- src/main/java/org/example/Respoitory/RepoProductosCarritos.java
+++ src/main/java/org/example/Respoitory/RepoProductosCarritos.java
@@ -1,11 +1,9 @@
 package org.example.Respoitory;
 
 import org.example.model.ProductoCarrito;
-import org.omg.CORBA.INTERNAL;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RestController;
 
 @CrossOrigin
 @RepositoryRestResource(excerptProjection = inlinesProductoCarrito.class)
